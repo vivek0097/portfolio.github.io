6418603d2ba6a6e9db8865994cfdad2ba76d2977
Vivek Singh <viveksingh96007@gmail.com>
2022-01-27 20:14:08 +0530

    file updated

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
 $(document).ready(function() {
 
-  $("#menu").click(function() {
-    $(this).toggleClass("fa-times");
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on("scroll load",function(){
-    $("#menu").removeClass("fa-times");
-    $("header").toggleClass("toggle");
-    
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
-  });
-
+  
   //smooth scrolling
 
   $('a[href*="#"]').on("click", function(e) {
